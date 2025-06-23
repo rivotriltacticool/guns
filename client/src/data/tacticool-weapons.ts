490605b23b6b4d1ba1aeb91ed474d9fdd822d36e
apptxnhx <elekfkn@gmail.com>
2025-06-23 03:05:50 +0100

    update fa

--- client/src/data/tacticool-weapons.ts
+++ client/src/data/tacticool-weapons.ts
@@ -2851,7 +2851,7 @@ export const tacticoolWeapons: Record<string, Weapon[]> = {
         { icon: "fas fa-fist-raised", label: "Dano Melee", value: "930" },
         { icon: "fas fa-bolt", label: "Tempo de Recarga", value: "2.3" },
         { icon: "fas fa-ruler-horizontal", label: "Distancia da Tração", value: "11" },
-        { icon: "fas fa-meteor", label: "Velocidade do Ativo", value: "45" },
+        { icon: "fas fa-regular fa-meteor", label: "Velocidade do Ativo", value: "45" },
         { icon: "fas fa-dumbbell", label: "Força de Tração", value: "1400" },
         { icon: "fas fa-running", label: "Velocidade", value: "85" }
       ]
